6ae041705a8e8aefe72d59fc3ce360077aef3ad6
kylej <kylej@LAPTOP-PR31M6RO>
2024-06-10 20:40:58 +0200

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SeleniumC_
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
