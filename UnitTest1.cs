96512e85c6fa3fdb63d15f86becf3a5835371651
kylej <kylejulie44@gmail.com>
2024-06-10 21:12:25 +0200

    -Chrome driver setup

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,16 +1,29 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace SeleniumC_
 {
     public class Tests
     {
+        public IWebDriver _driver;
+
         [SetUp]
         public void Setup()
         {
+            var driverPath = Environment.GetEnvironmentVariable("ChromeDriverPath");
+            _driver = new ChromeDriver(driverPath);
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            _driver.Navigate().GoToUrl("https://webdriveruniversity.com/index.html");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Close();
         }
     }
 }
